9f6f171efbfb0ccc5b2ab214f83a1647b3b8c92b
MauriceGilmore21 <63563101+MauriceGilmore21@users.noreply.github.com>
2020-04-19 18:20:06 +0000

    My Second Project Week 13

---
+++ MySecondProject/App copy 2.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+export default function MyApp() {
+
+function getFullName(fName,mName,lName){
+  return fName + " " + mName + " " + lName;
+}
+
+  const pet = "Dog";
+  return (
+    
+      <Text>
+        {"\n\n\n\n\n\n"}
+        Hello, I am a student in CIS340! {"\n"}
+        My full name is {getFullName("Maurice", "Lorenzo", "Gilmore")} {"\n"} 
+        I have a {pet}!
+      </Text>
+    
+  );
+}
+
+
